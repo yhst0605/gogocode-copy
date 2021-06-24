e25f6bd1054dc0da41f9a87de3a0260a13d921b1
zhouyuhang <zhouyuhang0605@163.com>
2021-06-24 20:22:26 +0800

    first commit

---
+++ packages/esbuild-import-plugin/index.js
@@ -0,0 +1,185 @@
+const gogocode = require('gogocode')
+const fs = require('fs')
+const kebabCase = require('lodash/kebabCase')
+const path = require('path')
+
+// const replacer = kebabCase
+// 测试用例:
+// - 使用引用
+// - 直接使用
+// - 重复 —— 去重
+const getUsedComponents = (source, libName) => {
+  const components = []
+  const usedComponents = source.find(`${libName}.$_$`)
+  usedComponents.each(ast => {
+    const componentName = ast.match[0][0].value
+    const parentNode = ast.parent()
+    components.push(componentName)
+    switch (parentNode.node.type) {
+      case 'VariableDeclarator': {
+        /**
+         * const Button = Antd.Button -> N/A
+         */
+        let removedFlag = false
+        ast.parents().each(parentNode => {
+          switch (parentNode.value.type) {
+            case 'VariableDeclaration': {
+              if (removedFlag) {
+                return
+              }
+              parentNode.remove()
+              removedFlag = true
+            }
+          }
+        })
+        break
+      }
+      default: {
+        /**
+         * <Antd.Select /> -> <Select />
+         */
+        ast.replaceBy(componentName)
+      }
+    }
+  })
+
+  return [...new Set(components)]
+}
+
+const pluginImport = (options = {}) => ({
+  name: 'esbuild-plugin-import',
+  setup(build) {
+    const {
+      filter = /\.(jsx|tsx|js|ts)$/,
+      namespace = 'file',
+      options: importOptions = [],
+    } = options
+    const transformContents = ({ contents, args }) => {
+      const { path: filePath } = args
+
+      const ext = path.extname(filePath).slice(1)
+      return new Promise((resolve, reject) => {
+        try {
+          const source = gogocode(contents)
+
+          source
+            .find([`import {$$$1} from '$_$1'`, `import $$$1 from '$_$1'`])
+            .each(ast => {
+              const [libAst] = ast.match[1]
+              const libraryName = libAst.value
+              const option = importOptions.find(
+                importOption => importOption.libraryName === libraryName,
+              )
+              if (!option) {
+                return
+              }
+              const {
+                style,
+                libraryDirectory = 'lib',
+                camel2DashComponentName = 'true',
+                styleLibraryDirectory,
+                customStyleName,
+                customName,
+              } = option
+
+              const importStyle = (ast, component) => {
+                  if (!style && !styleLibraryDirectory) {
+                    return
+                  }
+                  const formatedComponentName = camel2DashComponentName
+                    ? kebabCase(component)
+                    : component
+                  let finalCssPath
+
+                  if (customStyleName && typeof customStyleName === 'function') {
+                    finalCssPath = customStyleName(formatedComponentName)
+                    if (!finalCssPath) {
+                      return
+                    }
+                  } else {
+                    const libPath = `${libraryName}/${(styleLibraryDirectory || libraryDirectory) + '/'}${formatedComponentName}`
+                    let cssPath
+                    if (typeof style === 'function') {
+                      cssPath = style(libPath)
+                      if (!cssPath) {
+                        return
+                      }
+                    } else {
+                      cssPath = style === 'css' ? 'style/css' : 'style'
+                    }
+                    finalCssPath = `${libPath}/${cssPath}`
+                  }
+                  ast.after(
+                    `import '${finalCssPath}'\n`,
+                  )
+              }
+
+              const importComponent = (ast, component) => {
+                const formatedComponentName = camel2DashComponentName
+                  ? kebabCase(component)
+                  : component
+                let finalComponentPath
+                if (customName && typeof customName === 'function') {
+                  finalComponentPath = customName(formatedComponentName)
+                  if (!finalComponentPath) {
+                    return
+                  }
+                } else {
+                  finalComponentPath = `${libraryName}/${libraryDirectory + '/'}${formatedComponentName}/index`
+                }
+                ast.after(
+                  `import ${component} from '${finalComponentPath}'\n`,
+                )
+              }
+              const astReplace = importSpecifier => {
+                switch (importSpecifier.type) {
+                  case 'ImportDefaultSpecifier': {
+                    const localLibName = importSpecifier.local.name
+                    const components = getUsedComponents(source, localLibName)
+                    components.forEach(component => {
+                      importStyle(ast, component)
+                      importComponent(ast, component)
+                    })
+                    break
+                  }
+                  default: {
+                    const component = importSpecifier.local.name
+                    importStyle(ast, component)
+                    importComponent(ast, component)
+                  }
+                }
+              }
+
+              ast.match.$$$1 && ast.match.$$$1.forEach(astReplace)
+              ast.remove()
+            })
+
+          const result = source.generate()
+          resolve({
+            contents: result,
+            loader: ext.match(/j|tsx?$/) ? ext : 'js',
+          })
+        } catch (e) {
+          console.error(e)
+          reject(e)
+          // process.exit(1)
+        }
+      })
+    }
+
+    build.onLoad({ filter, namespace }, async args => {
+      try {
+        const contents = await fs.promises.readFile(args.path, 'utf8')
+        return transformContents({ args, contents })
+      } catch (e) {
+        console.error(e)
+        process.exit(1)
+      }
+    })
+  },
+})
+
+exports.default = pluginImport
+module.exports = pluginImport
+
+
---
+++ packages/gogocode-plugin-vue/src/render-function-api.js
@@ -0,0 +1,104 @@
+const scriptUtils = require('../utils/scriptUtils');
+const domProps = {
+  staticClass: {
+    key: 'class'
+  },
+  staticStyle: {
+    key: 'style'
+  },
+  on: '',
+  domProps: '',
+  props: '',
+  attrs: ''
+}
+
+module.exports = function (ast, options) {
+  // 迁移指南: https://v3.cn.vuejs.org/guide/migration/render-function-api.html
+  let scriptAst = ast.parseOptions && ast.parseOptions.language == 'vue' ? ast.find('<script></script>') : ast
+
+  if (scriptAst.length < 1) {
+    return ast
+  }
+  let hName = ''
+
+  // 搜集注册组件
+  let components = options && options.components ? options.components : []
+  scriptAst.find(`Vue.component($_$)`).each(c => {
+    let argumentList = c.attr('arguments')
+    if (Array.isArray(argumentList) && argumentList.length > 0) {
+      if (c.attr('arguments')[0].type == 'StringLiteral') {
+        components.push(c.attr('arguments')[0].value)
+        scriptUtils.addVueImport(scriptAst)
+      }
+    }
+  })
+
+  scriptAst.find([`render($_$){}`, `render: $_$ => $_$`]).each(node => {
+    let params = node.attr('params') || node.attr('value.params') || []
+    params.forEach((item, index) => {
+      hName = item.name
+      params.splice(index, 1);
+      scriptUtils.addVueImport(scriptAst)
+    })
+    node.find(`${hName}($_$,$_$,$_$)`).each(ast => {
+      let args = ast.attr('arguments') || []
+      args.forEach((arg, index) => {
+        if (arg.type == 'StringLiteral' && arg.value) {
+          ast.replace(`'${arg.value}'`, `Vue.resolveComponent('${arg.value}')`)
+        }
+        else if (index == 1 && arg.type == 'ObjectExpression') {
+          const plantRenderParaCode = `function plantRenderPara(params) {
+            const transProps = {
+              staticClass: "class",
+              staticStyle: "style",
+              on: "",
+              domProps: "",
+              props: "",
+              attrs: "",
+            };   
+            function obj2arr(obj) {
+              return typeof obj == "string"
+                ? [obj]
+                : Object.keys(obj).map((pk, index) => {
+                    return { [pk]: Object.values(obj)[index] };
+                  });
+            }
+            let result = {};
+            for (let key in params) {
+              if (transProps[key] == null) {
+                if (typeof params[key] == "object") {
+                  result[key] = obj2arr(params[key]);
+                } else {
+                  result[key] = params[key];
+                }
+              }
+            }
+            for (let key in params) {
+              if (transProps[key] === "") {
+                if (typeof params[key] == "object") {
+                  Object.assign(result, { ...params[key] });
+                }
+                else{
+                  result[key] = params[key];
+                }
+              }
+            }
+            for (let key in params) {
+              if (transProps[key]) {
+                result[transProps[key]] = result[transProps[key]] || [];
+                result[transProps[key]] = result[transProps[key]].concat(obj2arr(params[key]));
+              }
+            }
+            return result;
+          }`
+          ast.replace(ast.match[0][1].value ,`plantRenderPara(${ast.match[0][1].value})`)                  
+          if (!scriptAst.has(plantRenderParaCode)) {
+            scriptAst.after(plantRenderParaCode)
+          }
+        }
+      })
+      ast.attr('callee.name', 'Vue.h') 
+    })
+  })
+  return ast
+}
